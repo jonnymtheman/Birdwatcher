902e56b76282756d8c145741e5a2dcce731fa4c1
Jonas Nyman <c13jnn@cs.umu.se>
2016-08-11 19:49:22 +0200

    Stal en massa kod från boken, kunde inte visa kameran, var tvungen att enabla kameran för appen i Android. Imorgon ska kapitel 2 av kameran göras.

--- app/src/main/java/com/example/jonas/birdwatcher/BirdCameraFragment.java
+++ app/src/main/java/com/example/jonas/birdwatcher/BirdCameraFragment.java
@@ -1,7 +1,11 @@
 package com.example.jonas.birdwatcher;
 
+import java.io.IOException;
+import java.util.List;
+
 import android.annotation.TargetApi;
-import android.graphics.Camera;
+import android.hardware.Camera;
+import android.hardware.Camera.Size;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -13,77 +17,66 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
-import java.io.IOException;
-import java.lang.annotation.Target;
-
-/**
- * TODO Kolla deprication, välj kanske ett högre versionsnr för appen
- * TODO Kolla android.camera2
- * File:       ${FILE_NAME}.java
- * Author:     Jonas Nyman
- * Assignment: Inlämningsuppgift 3 - Valfri Applikation
- * Course:     Utveckling av mobila applikationer
- * Version:    1.0
- */
 public class BirdCameraFragment extends Fragment {
+    private static final String TAG = "BirdCameraFragment";
 
-    private Button takeButton;
+    private Camera mCamera;
     private SurfaceView mSurfaceView;
-    private android.hardware.Camera mCamera;
+    private View mProgressContainer;
 
 
     @Override
-    @SuppressWarnings("deprication")
-    public View onCreateView(LayoutInflater inflater, ViewGroup parent,
-                             Bundle savedInstanceState) {
-
+    @SuppressWarnings("deprecation")
+    public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_bird_camera, parent, false);
 
-        takeButton = (Button)v.findViewById(R.id.bird_camera_takePicture);
-        takeButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        mProgressContainer = v.findViewById(R.id.crime_camera_progressContainer);
+        mProgressContainer.setVisibility(View.INVISIBLE);
+        Button takePictureButton = (Button)v.findViewById(R.id.bird_camera_takePicture);
+        takePictureButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
                 getActivity().finish();
             }
         });
+
         mSurfaceView = (SurfaceView)v.findViewById(R.id.bird_camera_surfaceView);
-        final SurfaceHolder holder = mSurfaceView.getHolder();
+        SurfaceHolder holder = mSurfaceView.getHolder();
+        // deprecated, but required for pre-3.0 devices
         holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
         holder.addCallback(new SurfaceHolder.Callback() {
-            @Override
-            public void surfaceCreated(SurfaceHolder surfaceHolder) {
-                try{
+
+            public void surfaceCreated(SurfaceHolder holder) {
+                // tell the camera to use this surface as its preview area
+                try {
                     if (mCamera != null) {
                         mCamera.setPreviewDisplay(holder);
                     }
-                } catch (IOException e) {
-                    Log.e("CAMFRAG", "Error setting up preview", e);
+                } catch (IOException exception) {
+                    Log.e(TAG, "Error setting up preview display", exception);
                 }
             }
 
-            @Override
-            public void surfaceChanged(SurfaceHolder surfaceHolder, int format, int w, int h) {
-                 if (mCamera == null) {
-                     return;
-                 }
+            public void surfaceDestroyed(SurfaceHolder holder) {
+                // we can no longer display on this surface, so stop the preview.
+                if (mCamera != null) {
+                    mCamera.stopPreview();
+                }
+            }
 
-                android.hardware.Camera.Parameters parameters = mCamera.getParameters();
-                android.hardware.Camera.Size s = null;
+            public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
+                if (mCamera == null) return;
+
+                // the surface has changed size; update the camera preview size
+                Camera.Parameters parameters = mCamera.getParameters();
+                Size s = getBestSupportedSize(parameters.getSupportedPreviewSizes(), w, h);
                 parameters.setPreviewSize(s.width, s.height);
                 mCamera.setParameters(parameters);
                 try {
                     mCamera.startPreview();
                 } catch (Exception e) {
-                    Log.e("CAMFRAG", "Could not stard preview", e);
+                    Log.e(TAG, "Could not start preview", e);
                     mCamera.release();
                     mCamera = null;
-                }
-            }
-
-            @Override
-            public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
-                if (mCamera != null) {
-                    mCamera.stopPreview();
                 }
             }
         });
@@ -91,15 +84,15 @@ public class BirdCameraFragment extends Fragment {
         return v;
     }
 
-    // EGEN TRÅD SEN
     @TargetApi(9)
     @Override
     public void onResume() {
         super.onResume();
+
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.GINGERBREAD) {
-            mCamera = android.hardware.Camera.open(0);
+            mCamera = Camera.open(0);
         } else {
-            mCamera = android.hardware.Camera.open();
+            mCamera = Camera.open();
         }
     }
 
@@ -113,5 +106,20 @@ public class BirdCameraFragment extends Fragment {
         }
     }
 
+    /** a simple algorithm to get the largest size available. For a more
+     * robust version, see CameraPreview.java in the ApiDemos
+     * sample app from Android. */
+    private Size getBestSupportedSize(List<Size> sizes, int width, int height) {
+        Size bestSize = sizes.get(0);
+        int largestArea = bestSize.width * bestSize.height;
+        for (Size s : sizes) {
+            int area = s.width * s.height;
+            if (area > largestArea) {
+                bestSize = s;
+                largestArea = area;
+            }
+        }
+        return bestSize;
+    }
 
 }
--- app/src/main/java/com/example/jonas/birdwatcher/CameraActivity.java
+++ app/src/main/java/com/example/jonas/birdwatcher/CameraActivity.java
@@ -3,8 +3,20 @@ package com.example.jonas.birdwatcher;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class CameraActivity extends SingleFragmentActivity {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        // hide the window title.
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        // hide the status bar and other OS-level chrome
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        super.onCreate(savedInstanceState);
+    }
 
     @Override
     protected Fragment createFragment() {
